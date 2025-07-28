f7198c511f6999554609bbbdaade60f0b400ce64
Ali Hesham Abdelhady <ali04hesham@gmail.com>
2025-07-28 18:26:42 +0300

    latest

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductslistComponent } from './productslist/productslist.component';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NavbarComponent, ProductslistComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
---
+++ my-app/src/app/productslist/productslist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productslist',
+  imports: [],
+  templateUrl: './productslist.component.html',
+  styleUrl: './productslist.component.css'
+})
+export class ProductslistComponent {
+
+}
