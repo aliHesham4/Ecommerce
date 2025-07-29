28e381935d2f4b43b116f59b822e0e723d05e3e8
Ali Hesham Abdelhady <ali04hesham@gmail.com>
2025-07-29 17:24:13 +0300

    footer

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,7 +10,7 @@ import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, NavbarComponent, ProductslistComponent, FooterComponent],
+  imports: [CommonModule,RouterModule, RouterOutlet, HeaderComponent, NavbarComponent, ProductslistComponent, FooterComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductslistComponent } from './productslist/productslist.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:NavbarComponent},
+    {path:'products/',component:ProductslistComponent},
+    {path:'products',redirectTo:'products/'}
+];
