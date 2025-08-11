98dda9269af123ccd5c5014188f372266aef63fb
Ali Hesham Abdelhady <ali04hesham@gmail.com>
2025-08-11 19:51:41 +0300

    login and register are functional

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../Done/footer/footer.component';
 import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { FormGroup,FormBuilder,ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FooterComponent,RouterLink],
+  imports: [FooterComponent,RouterLink,CommonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rememberMe: [false] // Optional field for "Remember Me" functionality
+    });
+  }
 
-  constructor(private router: Router) {}
+  onSubmit() {
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    if (localStorage.getItem(email) === password) {
+      console.log('Login successful');
+      this.router.navigate(['/products']); 
+    } else {
+      this.loginForm.get('email')?.setErrors({ emailExists: true });
+      console.log('Email or password is incorrect');
+    }
+  }
+
 
 }
--- my-app/src/app/register/register.component.ts
+++ my-app/src/app/register/register.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../Done/footer/footer.component';
 import { Router, RouterLink } from '@angular/router';
+import { FormGroup, FormBuilder, Validators,ReactiveFormsModule,} from '@angular/forms';
+import { disableDebugTools } from '@angular/platform-browser';
+import { json } from 'stream/consumers';
 
 @Component({
   selector: 'app-register',
-  imports: [FooterComponent, RouterLink],
+  imports: [FooterComponent, RouterLink,ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
- 
-   constructor(private router: Router) {}
+    registerForm!: FormGroup;
+   constructor(private formBuilder: FormBuilder,private router: Router) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.get('password')?.value !== this.registerForm.get('confirmPassword')?.value) {
+      this.registerForm.get('confirmPassword')?.setErrors({ mismatch: true });
+
+    }else if (localStorage.getItem(this.registerForm.get('email')?.value)!== null) {
+      this.registerForm.get('email')?.setErrors({ emailExists: true });
+    }
+     else if (this.registerForm.valid) {
+       localStorage.setItem(this.registerForm.get('email')?.value, this.registerForm.get('password')?.value);
+
+      console.log('Form Submitted', this.registerForm.value);
+      this.router.navigate(['/login']); // Navigate to login page after successful registration
+    } else {
+      console.log('Form is invalid');
+    }
+
+    
+     
+
+  
+  }
+
 }
